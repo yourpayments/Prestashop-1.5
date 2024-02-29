ca8a01515373be3f3fafd4a331db583db705a707
Grigoriy Efimov <grigoriy.efimov@ypmn>
2024-02-29 14:05:23 +0300

    Change payu.ru to ypmn.ru

--- payu/payu.php
+++ payu/payu.php
@@ -49,7 +49,7 @@ class payu extends PaymentModule
 	private function _displayForm()
 	{
 		$ipn_url    = 'http://'. $_SERVER['HTTP_HOST'] .'/modules/payu/result.php';
-		$system_url = $this->Payu_getVar('system_url') ?: 'https://secure.payu.ru/order/lu.php';
+		$system_url = $this->Payu_getVar('system_url') ?: 'https://secure.ypmn.ru/order/lu.php';
 		$back_ref   = $this->Payu_getVar('back_ref')   ?: 'http://'. $_SERVER['HTTP_HOST'] .'/modules/payu/back.php';
 
 		$this->_html .=
